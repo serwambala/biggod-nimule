e3801ed2bdebbf773eac954b4e47d818cac46a6a
serwambala <dzerwa88@gmail.com>
2026-05-14 16:16:07 +0300

    Prepare project for deployment

--- churchsite/settings.py
+++ churchsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cqi%_$f*(1h$%lnpw)xditkmn-es6141zcf8$)^3#(o(u--3zm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # EMAIL SETTINGS (DEVELOPMENT ONLY)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -118,4 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [BASE_DIR / 'static']
