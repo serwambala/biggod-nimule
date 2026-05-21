fc6aae5c80e39d6afce3ad6325dfa045c58686ae
serwambala <dzerwa88@gmail.com>
2026-05-21 21:59:18 +0300

    edited the number of displaying approved testimonies from views.py to 5

---
+++ pages/urls-DESKTOP-1ULGF16.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('church/', views.church, name='church'),
+    path('children/', views.children, name='children'),
+    path('get-involved/', views.get_involved, name='get_involved'),
+    path('about/', views.about, name='about'),
+    path('core-values/', views.core_values, name='core-values'),
+    path('vision/', views.vision, name='vision_&_mission'),
+    path('story/', views.story, name='story'),
+    path('contact/', views.contact, name='contact'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('statement-of-faith/', views.statement_of_faith, name='statement-of-faith'),
+    path('leadership/', views.leadership, name='leadership'),
+    path('sermon/<int:id>/', views.sermon_detail, name='sermon_detail'),
+]
---
+++ pages/views-DESKTOP-1ULGF16.py
@@ -0,0 +1,111 @@
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def church(request):
+    return render(request, 'church.html')
+
+def children(request):
+    return render(request, 'children.html')
+
+def get_involved(request):
+    return render(request, 'get_involved.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def story(request):
+    return render(request, 'story.html')
+
+def vision(request):
+    return render(request, 'vision.html')
+
+def core_values(request):
+    return render(request, 'core-values.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def leadership(request):
+    return render(request, 'leadership.html')
+
+def statement_of_faith(request):
+    return render(request, 'statement-of-faith.html')
+
+
+from django.http import HttpResponse
+from .models import ContactMessage
+
+def contact(request):
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        ContactMessage.objects.create(
+            name=name,
+            email=email,
+            message=message
+        )
+
+        return HttpResponse("Message received. Thank you for contacting us.")
+
+    return render(request, "contact.html")
+
+from django.shortcuts import render, redirect
+
+from .models import (
+    Event,
+    Testimony,
+    Sermon
+)
+
+
+def church(request):
+
+    # HANDLE TESTIMONY SUBMISSION
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+
+        message = request.POST.get('message')
+
+        Testimony.objects.create(
+            name=name,
+            message=message
+        )
+
+        return redirect('church')
+
+
+    # GET MOST RECENT SERMON
+    latest_sermon = Sermon.objects.order_by('-created_at').first()
+
+    # GET EVENTS
+    events = Event.objects.all().order_by('date')
+
+
+    # GET APPROVED TESTIMONIES
+    testimonies = Testimony.objects.filter(
+        approved=True
+    ).order_by('-created_at')[:5]
+
+
+    context = {
+        'latest_sermon': latest_sermon,
+        'events': events,
+        'testimonies': testimonies,
+    }
+
+    return render(
+        request,
+        'church.html',
+        context
+    )
+def sermon_detail(request, id):
+    sermon = Sermon.objects.get(id=id)
+    return render(request, 'sermon_detail.html', {'sermon': sermon})
