39f677b824103cc6b8cd91d1147411ec267bf46f
serwambala <dzerwa88@gmail.com>
2026-05-21 16:25:35 +0300

    reconfigured databases

--- churchsite/settings.py
+++ churchsite/settings.py
@@ -87,8 +87,10 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
