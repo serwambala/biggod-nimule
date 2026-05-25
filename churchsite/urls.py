43732ff54518e75d1a4283f66d201339ae4eda8d
serwambala <dzerwa88@gmail.com>
2026-05-25 14:25:45 +0300

    Added robots.txt

--- churchsite/urls.py
+++ churchsite/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 from django.contrib.sitemaps.views import sitemap
 from pages.sitemaps import StaticViewSitemap
-
+from django.views.generic import TemplateView
 sitemaps = {
     'static': StaticViewSitemap,
 }
@@ -33,6 +33,13 @@ urlpatterns = [
     sitemap,
     {'sitemaps': sitemaps},
     name='django.contrib.sitemaps.views.sitemap'
+),
+    path(
+    "robots.txt",
+    TemplateView.as_view(
+        template_name="robots.txt",
+        content_type="text/plain"
+    ),
 ),
 ]
 
